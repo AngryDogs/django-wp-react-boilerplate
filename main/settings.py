4509a522a1fe9074e06a27a97b2685b69f44ce2a
AngryDogs <rain.vink@gmail.com>
2016-07-05 16:56:31 +0300

    Fixed deprecated core

--- main/settings.py
+++ main/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.core.context_processors.static',
+                'django.template.context_processors.static',
                 'django.template.context_processors.media',
             ],
         },
